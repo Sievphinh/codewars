dfae3e2ec1ac8ed92e6c210a3a6b304691bea25a
wayne <sievphygn@gmail.com>
2022-06-15 07:10:16 +0700

    add something stupid

--- 4kyu/snailsort/index.js
+++ 4kyu/snailsort/index.js
@@ -1,4 +1,4 @@
-snail = function (arr) {
+snail = function(arr) {
   let newArr = arr[0];
   if (arr.lenght === 1) return arr[0];
   return newArr.concat(snail(transposeArr(arr.slice(1))));
@@ -7,3 +7,5 @@ snail = function (arr) {
 function transposeArr(arr) {
   return arr[0].map((_, index) => arr.map((row) => row[index])).reverse();
 }
+
+console.log('hello world')
